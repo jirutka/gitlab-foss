6269575346c93d4f6d9f333b72ad7c8a98c2ab7b
Jakub Jirutka <jakub@jirutka.cz>
2017-09-12 12:30:36 +0200

    Improve AsciiDoc rendering - use asciidoctor-html5s

--- lib/gitlab/asciidoc/html5_converter.rb
+++ lib/gitlab/asciidoc/html5_converter.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
-require 'asciidoctor'
+require 'asciidoctor-html5s'
 
 module Gitlab
   module Asciidoc
-    class Html5Converter < (Asciidoctor::Converter.for 'html5')
+    class Html5Converter < Asciidoctor::Html5s::Converter
       register_for 'gitlab_html5'
 
-      def convert_stem(node)
+      # TODO: Return back "convert_" prefix after updating asciidoctor-html5s to >= 0.6.0.
+
+      def stem(node)
         return super unless node.style.to_sym == :latexmath
 
         %(<pre#{id_attribute(node)} data-math-style="display"><code>#{node.content}</code></pre>)
       end
 
-      def convert_inline_quoted(node)
+      def inline_quoted(node)
         return super unless node.type.to_sym == :latexmath
 
         %(<code#{id_attribute(node)} data-math-style="inline">#{node.text}</code>)
       end
 
-      def convert_inline_anchor(node)
+      def inline_anchor(node)
         node.id = "user-content-#{node.id}" if node.id && !node.id.start_with?('user-content-')
 
         super(node)
