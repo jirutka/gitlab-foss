8c40244f868dfe6ba782ac80652bd57ff23897d3
Jakub Jirutka <jakub@jirutka.cz>
2018-03-14 19:53:24 +0100

    Don't eager load google/apis

--- lib/google_api/cloud_platform/client.rb
+++ lib/google_api/cloud_platform/client.rb
@@ -1,8 +1,3 @@
-require 'google/apis/compute_v1'
-require 'google/apis/container_v1'
-require 'google/apis/cloudbilling_v1'
-require 'google/apis/cloudresourcemanager_v1'
-
 module GoogleApi
   module CloudPlatform
     class Client < GoogleApi::Auth
@@ -27,6 +22,14 @@ module GoogleApi
         def session_key_for_redirect_uri(state)
           "cloud_platform_second_redirect_uri_#{state}"
         end
+      end
+
+      def initialize
+        require 'google/apis/compute_v1'
+        require 'google/apis/container_v1'
+        require 'google/apis/cloudbilling_v1'
+        require 'google/apis/cloudresourcemanager_v1'
+        super
       end
 
       def scope
