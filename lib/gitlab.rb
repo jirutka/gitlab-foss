1ea204bc5a934f02dbe28ead50447f4755f6fce5
Jakub Jirutka <jakub@jirutka.cz>
2018-09-27 00:13:48 +0200

    Don't try to read GitLab revision using git

--- lib/gitlab.rb
+++ lib/gitlab.rb
@@ -24,13 +24,7 @@ module Gitlab
       if File.exist?(root.join("REVISION"))
         File.read(root.join("REVISION")).strip.freeze
       else
-        result = Gitlab::Popen.popen_with_detail(%W[#{config.git.bin_path} log --pretty=format:%h --abbrev=11 -n 1])
-
-        if result.status.success?
-          result.stdout.chomp.freeze
-        else
-          "Unknown"
-        end
+        "Unknown".freeze
       end
     end
   end
