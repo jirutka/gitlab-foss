16e164dd0a99c182116c1d6846e3256644cc4921
Jakub Jirutka <jakub@jirutka.cz>
2018-09-28 21:22:57 +0200

    Remove Clusters/Kubernetes from UI and routes

--- config/routes/google_api.rb
+++ config/routes/google_api.rb
@@ -1,6 +1,6 @@
 scope '-' do
   namespace :google_api do
-    resource :auth, only: [], controller: :authorizations do
+    resource :auth, only: [], controller: :authorizations, constraints: proc { false } do  # patched
       match :callback, via: [:get, :post]
     end
   end
